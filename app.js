79b73735e6e43fec31d14ac723c0402c25e3d446
Tselate <Tselatedawit@outlook.com>
2021-08-11 11:29:54 -0600

    Initial

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-let addButton = document.getElementById("add")
-let subButton = document.getElementById("subtract")
+//Add buttons
+let addPear = document.getElementById("addPear")
+let addApple = document.getElementById("addApple")
+let addBanana = document.getElementById("addBanana")
+
+//Subtract buttons 
+let subPear = document.getElementById("subtractPear")
+let subApple = document.getElementById("subtractApple")
+let subBanana = document.getElementById("subtractBanana")
+
+//Number of firuts 
+let numPears = document.getElementById("num-pears")
+let numApples = document.getElementById("num-apples")
+let numBanana = document.getElementById("num-bananas")
 
 let count = 0
 
-addButton.addEventListener("click", function addFirut (){
+//Add functions 
+addPear.addEventListener("click", function increasePear (){
     count += 1
-    console.log(count)
+    if (count === 1) {
+        numPears.innerHTML= count + " Pear"
+    } else {
+        numPears.innerHTML= count + " Pears"
+    } 
+})
+
+addApple.addEventListener("click", function increaseApple (){
+    count += 1
+    if (count === 1) {
+        numApples.innerHTML= count + " Apple"
+    } else {
+        numApples.innerHTML= count + " Apples"
+    } 
+})
+
+addBanana.addEventListener("click", function increaseBanana (){
+    count += 1
+    if (count === 1) {
+        numBanana.innerHTML= count + " Banana"
+    } else {
+        numBanana.innerHTML= count + " Bananas"
+    } 
 })
