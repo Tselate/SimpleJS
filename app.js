07d26c6830364cb3c63a7201ed82d321126e3bd9
Tselate <Tselatedawit@outlook.com>
2021-08-11 11:08:30 -0600

    Initial

---
+++ app.js
@@ -0,0 +1,9 @@
+let addButton = document.getElementById("add")
+let subButton = document.getElementById("subtract")
+
+let count = 0
+
+addButton.addEventListener("click", function addFirut (){
+    count += 1
+    console.log(count)
+})
